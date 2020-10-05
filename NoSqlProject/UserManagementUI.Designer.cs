de1d6775b3de0671c62f9d3750807c2f91ed73d8
egecnr <54850459+egecnr@users.noreply.github.com>
2020-10-05 20:38:22 +0200

    Finished User management part- ege

--- NoSqlProject/UserManagementUI.Designer.cs
+++ NoSqlProject/UserManagementUI.Designer.cs
@@ -35,6 +35,10 @@
             this.FirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.LastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Tickets = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lbl = new System.Windows.Forms.Label();
+            this.cmbType = new System.Windows.Forms.ComboBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnAdd
@@ -88,11 +92,51 @@
             this.Tickets.Text = "#Tickets";
             this.Tickets.Width = 200;
             // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl.Location = new System.Drawing.Point(28, 52);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(99, 20);
+            this.lbl.TabIndex = 3;
+            this.lbl.Text = "Search by:";
+            // 
+            // cmbType
+            // 
+            this.cmbType.FormattingEnabled = true;
+            this.cmbType.Location = new System.Drawing.Point(138, 52);
+            this.cmbType.Name = "cmbType";
+            this.cmbType.Size = new System.Drawing.Size(121, 24);
+            this.cmbType.TabIndex = 4;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(138, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(121, 22);
+            this.txtSearch.TabIndex = 5;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Filter:";
+            // 
             // UserManagementUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.cmbType);
+            this.Controls.Add(this.lbl);
             this.Controls.Add(this.userView);
             this.Controls.Add(this.btnAdd);
             this.Name = "UserManagementUI";
@@ -100,6 +144,7 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UserManagementUI_FormClosing);
             this.Load += new System.EventHandler(this.UserManagementUI_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -111,5 +156,9 @@
         private System.Windows.Forms.ColumnHeader FirstName;
         private System.Windows.Forms.ColumnHeader LastName;
         private System.Windows.Forms.ColumnHeader Tickets;
+        private System.Windows.Forms.Label lbl;
+        private System.Windows.Forms.ComboBox cmbType;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
     }
 }
--- NoSqlProject/UserManagementUI.cs
+++ NoSqlProject/UserManagementUI.cs
@@ -26,6 +26,131 @@ namespace NoSQLProject
         {
             allUsers = service.getAllUsers();
             FillListView();
+            FillComboBox();
+        }
+
+
+        // Determines the filtering type
+        private void filterUsers()
+        {
+            switch(cmbType.SelectedIndex)
+            {
+                case 0:
+                    FilterById();
+                    break;
+                case 1:
+                    FilterByEmail();
+                    break;
+                case 2:
+                    FilterByFName();
+                    break;
+                case 3:
+                    FilterByLName();
+                    break;
+                case 4:
+                    FilterByTickets(); 
+                    break;
+                default:
+                    FilterByEmail();
+                    break;
+            }
+            if (String.IsNullOrWhiteSpace(txtSearch.Text))
+            {
+                FillListView();
+            }
+        }
+
+        //filters by first name
+        private void FilterByFName()
+        {
+            userView.Items.Clear();
+
+            foreach (User u in allUsers)
+            {
+                if (u.firstName.ToLower().Contains(txtSearch.Text.ToLower()))
+                {
+                    AddUserToListView(u);
+                }
+            }
+        }
+        //filters by email
+        private void FilterByEmail()
+        {
+            userView.Items.Clear();
+
+            foreach (User u in allUsers)
+            {
+                if (u.email.ToLower().Contains(txtSearch.Text.ToLower()))
+                {
+                    AddUserToListView(u);
+                }
+            }
+        }
+
+        private void AddUserToListView(User u)
+        {
+            ListViewItem item = new ListViewItem(
+                         new string[] {
+                        u.id.ToString(),
+                        u.email,
+                        u.firstName,
+                        u.lastName,
+                        u.nrTickets.ToString()
+                     });
+            userView.Items.Add(item);
+        }
+        //filters by last name
+        private void FilterByLName()
+        {
+            userView.Items.Clear();
+
+            foreach (User u in allUsers)
+            {
+                if (u.lastName.ToLower().Contains(txtSearch.Text.ToLower()))
+                {
+                    AddUserToListView(u);
+                }
+            }
+        }
+
+        //filters by id
+        private void FilterById()
+        {
+            userView.Items.Clear();
+
+            foreach(User u in allUsers)
+            {
+                if (u.id.ToString().ToLower().Contains(txtSearch.Text.ToLower()))
+                {
+                    AddUserToListView(u);
+                }
+            }
+            
+        }
+
+
+        //filters by ticket numbers
+
+        private void FilterByTickets()
+        {
+            userView.Items.Clear();
+
+            foreach (User u in allUsers)
+            {
+                if (u.nrTickets.ToString().ToLower().Contains(txtSearch.Text.ToLower()))
+                {
+                    AddUserToListView(u);
+                }
+            }
+        }
+
+        
+
+        private void FillComboBox()
+        {
+            string[] items = new string[] {"Id","Email","First name","Last name","Number of tickets" };
+            cmbType.Items.AddRange(items);
+            cmbType.SelectedIndex= 1;//It starts from email by default
         }
 
         private void FillListView()
@@ -39,21 +164,9 @@ namespace NoSQLProject
 
             foreach(User u in allUsers)
             {
-                ListViewItem item = new ListViewItem(
-                        new string[] {
-                        u.id.ToString(),
-                        u.email,
-                        u.firstName,
-                        u.lastName,
-                        u.nrTickets.ToString()
-                    });                                 
-                userView.Items.Add(item);
+                AddUserToListView(u);
             }
-
-            userView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
-           
-           
-
+            userView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);                      
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
@@ -65,6 +178,11 @@ namespace NoSQLProject
         private void UserManagementUI_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            filterUsers();
         }
     }
 }
