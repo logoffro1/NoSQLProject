b70f1175c86d079fe2ecd4c6abb86605cd013388
Ilie Cosmin <cosmin.ilie2704@gmail.com>
2020-10-17 13:41:57 +0200

    Improved tickets search

--- NoSQLProject/SearchTickets.cs
+++ NoSQLProject/SearchTickets.cs
@@ -1,19 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows.Forms;
 
 namespace NoSQLProject
 {
     class SearchTickets
     {
-        public SearchTickets(TextBox searchBox, ListView listViewTickets, ListView allTickets)
+        private List<ListViewItem> items;
+        private ListView listViewTickets;
+        private ListView allTickets;
+        public SearchTickets(TextBox searchBox, ListView listViewTickets, ListView allTickets) // I will add comments later
         {
+            this.listViewTickets = listViewTickets;
+            this.allTickets = allTickets;
             string[] searchBoxOptions = searchBox.Text.Split(' ');
 
-            List<ListViewItem> items = new List<ListViewItem>();
+            items = new List<ListViewItem>();
 
             if (searchBoxOptions.Length >= 3)
             {
@@ -24,67 +25,67 @@ namespace NoSQLProject
                         for (int i = 0; i < searchBoxOptions.Length; i++)
                         {
                             if (i == 1) continue;
-
-                            for (int j = 0; j < item.SubItems.Count; j++)
-                                if (item.SubItems[j].Text.ToLower().Contains(searchBoxOptions[i].ToLower()))
-                                {
-                                    if (!items.Contains(item))
-                                        items.Add(item);
-                                    break;
-                                }
+                            CheckIfSubitemContainsText(item, searchBoxOptions[i]);
                         }
                     }
                 }
                 else if (searchBoxOptions[1] == "&")
-                {
-                    foreach (ListViewItem item in allTickets.Items)
-                    {
-                        for (int i = 0; i < searchBoxOptions.Length; i++)
-                        {
-                            bool containsFirst = false;
-                            if (i == 1) continue;
-
-                            for (int j = 0; j < item.SubItems.Count; j++)
-                            {
-                                if (item.SubItems[j].Text.ToLower().Contains(searchBoxOptions[i].ToLower()))
-                                {
-                                    containsFirst = true;
-                                }
-                                else if (item.SubItems[j].Text.ToLower().Contains(searchBoxOptions[2].ToLower()))
-                                {
-                                    if (containsFirst)
-                                    {
-                                        if (!items.Contains(item))
-                                            items.Add(item);
-                                        break;
-
-                                    }
-                                }
-
-                            }
-
-                        }
-                    }
-                }
+                    CheckIfSubitemContainsBothFilters(searchBoxOptions);
             }
             else
             {
                 foreach (ListViewItem item in allTickets.Items)
-                {
-                    for (int j = 0; j < item.SubItems.Count; j++)
-                        if (item.SubItems[j].Text.ToLower().Contains(searchBox.Text.ToLower()))
-                        {
-                            if (!items.Contains(item))
-                                items.Add(item);
-                            break;
-                        }
-                }
+                    CheckIfSubitemContainsText(item, searchBox.Text);
             }
 
+            FillTicketList();
+
+        }
+        private void CheckIfSubitemContainsText(ListViewItem item, string text)
+        {
+
+            for (int j = 0; j < item.SubItems.Count; j++)
+                if (item.SubItems[j].Text.ToLower().Contains(text.ToLower()))
+                {
+                    if (!items.Contains(item))
+                        items.Add(item);
+                    break;
+                }
+        }
+        private void FillTicketList()
+        {
             listViewTickets.Items.Clear();
             foreach (ListViewItem item in items)
                 listViewTickets.Items.Add((ListViewItem)item.Clone());
+        }
+        private void CheckIfSubitemContainsBothFilters(string[] searchBoxOptions)
+        {
+            foreach (ListViewItem item in allTickets.Items)
+            {
+                for (int i = 0; i < searchBoxOptions.Length; i++)
+                {
+                    bool containsFirst = false;
+                    if (i == 1) continue;
 
+                    for (int j = 0; j < item.SubItems.Count; j++)
+                    {
+                        if (item.SubItems[j].Text.ToLower().Contains(searchBoxOptions[i].ToLower()))
+                            containsFirst = true;
+                        else if (item.SubItems[j].Text.ToLower().Contains(searchBoxOptions[2].ToLower()))
+                        {
+                            if (containsFirst)
+                            {
+                                if (!items.Contains(item))
+                                    items.Add(item);
+                                break;
+
+                            }
+                        }
+
+                    }
+
+                }
+            }
         }
     }
 }
